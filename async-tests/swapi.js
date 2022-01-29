26694884c9ae0c503d5a4d8ab75b7a817094d694
gustavonikov <gustavonikov@gmail.com>
2022-01-29 07:49:19 -0300

    docs: adding js docs for async functions

--- async-tests/swapi.js
+++ async-tests/swapi.js
@@ -1,6 +1,36 @@
 // Now a little bit more realistic examples. To this second example we gonna divide in 3 test files.
 const fetch = require('node-fetch')
 
+/**
+ * @typedef Character
+ * @property {string} name
+ * @property {string} height
+ * @property {string} mass
+ * @property {string} hair_color
+ * @property {string} eye_color
+ * @property {string} birth_year
+ * @property {string} gender
+ * @property {string} homeworld
+ * @property {string[]} films
+ * @property {string[]} species
+ * @property {string[]} vehicles
+ * @property {string[]} starships
+ * @property {string} created
+ * @property {string} edited
+ * @property {string} url
+ */
+
+/**
+ * @typedef CharactersData
+ * @property {number} count
+ * @property {Character[]} results
+ */
+
+/**
+ * Get all Stars Wars characters from "swapi" API.
+ *
+ * @return  {Promise<CharactersData>} 
+ */
 const getStarWarsCharacters = async () => {
   try {
     const charactersData = await fetch('https://swapi.dev/api/people')
@@ -17,6 +47,13 @@ const getStarWarsCharacters = async () => {
   }
 }
 
+/**
+ * Get a Star Wars character by name.
+ *
+ * @param   {string}  characterName  - E.g: 'Luke' or 'Luke Skywalker'.
+ *
+ * @return  {Promise<Character>}            
+ */
 const getStarWarsCharacter = async (characterName) => {
   try {
     const { results: characters } = await getStarWarsCharacters()
